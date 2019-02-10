50283a6622a3073cc9d9ff33e7472b2a7da7ad37
anmolduggal <1606087@kiit.ac.in>
2019-02-10 16:27:56 +0530

    UPLOAD

--- models/rating.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-var RatingSchema = new mongoose.Schema({
-		_creator : { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-		rating:{type:Number,default:0,min:0,max:5},
-		ratingdate:{type:Date,default:Date.now}
-	});
-module.exports = mongoose.model("Rating", RatingSchema);
